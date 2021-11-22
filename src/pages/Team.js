dd84bd53c017483417d4cc3892f442a8c637bd00
Patrickodey360coder <patrickodey419@gmail.com>
2021-11-22 16:31:59 +0100

    team and contact us

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -5,12 +5,17 @@ import team_2 from '../images/team-1.jpeg'
 function Team() {
   return (
     <div className="container my-5 py-5" id="team">
-      <div className="row">
+      <h1 className="text-center mb-5">Meet the team</h1>
+      <div className="row text-center">
         <div className="col-12 col-sm-12 col-md-4 col-lg-4 mx-auto">
-          <img src={team_1} alt={team_1} className="img-fluid team-img" />
+          <img src={team_1} alt={team_1} className="img-fluid team-img mb-2" />
+          <h4>Patrick odey</h4>
+          <p>CEO</p>
         </div>
         <div className="col-12 col-sm-12 col-md-4 col-lg-4 mx-auto">
-          <img src={team_2} alt={team_2} className="img-fluid team-img" />
+          <img src={team_2} alt={team_2} className="img-fluid team-img mb-2" />
+          <h4>Patrick odey</h4>
+          <p>CEO</p>
         </div>
       </div>
     </div>
