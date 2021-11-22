4f1f76f0d4b85d6076f6f5f337cf8c46cff93d1d
Patrickodey360coder <patrickodey419@gmail.com>
2021-11-22 06:43:12 +0100

    added logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Nav, Navbar} from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
+import logo from '../images/logo.png'
 
 
 function Navs() {
   return (
        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
         <Container>
-        <Navbar.Brand href="/" className="orange"><strong>Akshiptika</strong></Navbar.Brand>
+        {/* <Navbar.Brand href="/" className="orange"><strong>Akshiptika</strong></Navbar.Brand> */}
+        <Navbar.Brand href="/" className="orange"><img src={logo} alt={logo} class="img-fluid logo" /></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
