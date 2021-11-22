6c3d824dbe9122c4a7752aadb1da8c58cc6b104e
Patrickodey360coder <patrickodey419@gmail.com>
2021-11-22 18:57:58 +0100

    team and contact us

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import img from '../images/contact.svg'
 
 function Contact() {
+  const [forms, setForms] = useState({});
+  const [form, setForm] = useState('');
+
+  function handleSubmit(e){
+    e.preventDefault();
+    setForm(forms)
+    console.log(form)
+  }
   return (
     <div id="contact">
       <div className="container py-5">
@@ -10,15 +18,15 @@ function Contact() {
             <img src={img} alt={img} className="img-fluid contact-img" />
           </div>
           <div className="col-12 col-lg-6 col-md-6">
-            <h1 className="text-center mb-4">Contact Us</h1>
+            <h1 className="text-center my-3 mb-4">Contact Us</h1>
             <div className="row">
               <div className="col-10 col-sm-8 col-md-10 col-md-10 mx-auto">
               <form>
                 <div className="mb-3">
-                  <input className="form-control form-control-lg" type="text" placeholder="name..." aria-label=".form-control-lg example"/>
+                  <input className="form-control form-control-lg" type="text" placeholder="name..." aria-label=".form-control-lg example" value={form}  />
                 </div>
                 <div className="mb-3">
-                  <input type="email" class="form-control form-control-lg" id="exampleFormControlInput1" placeholder="email"/>
+                  <input type="email" class="form-control form-control-lg" id="exampleFormControlInput1" placeholder="email" value={form} onChange={handleSubmit}/>
                 </div>
                 <div class="mb-3">
                   <textarea placeholder="message..." class="form-control form-control-lg" id="exampleFormControlTextarea1" rows="3"></textarea>
--- src/pages/Team.js
+++ src/pages/Team.js
@@ -6,16 +6,31 @@ function Team() {
   return (
     <div className="container my-5 py-5" id="team">
       <h1 className="text-center mb-5">Meet the team</h1>
-      <div className="row text-center">
-        <div className="col-12 col-sm-12 col-md-4 col-lg-4 mx-auto">
+      <div className="row text-center mx-auto">
+        <div className="col-12 col-sm-12 col-md-4 col-lg-4">
           <img src={team_1} alt={team_1} className="img-fluid team-img mb-2" />
-          <h5>Patrick odey</h5>
-          <p>CEO</p>
+          <h5>Munish Rana</h5>
+          <p className="text-muted font-italic">Chief Executive Officer</p>
         </div>
-        <div className="col-12 col-sm-12 col-md-4 col-lg-4 mx-auto">
+        <div className="col-12 col-sm-12 col-md-4 col-lg-4">
           <img src={team_2} alt={team_2} className="img-fluid team-img mb-2" />
-          <h5>Patrick odey</h5>
-          <p>CEO</p>
+          <h5>Olang A.O. Daniel</h5>
+          <p className="text-muted font-italic">Full Stack Developer</p>
+        </div>
+        <div className="col-12 col-sm-12 col-md-4 col-lg-4">
+          <img src={team_2} alt={team_2} className="img-fluid team-img mb-2" />
+          <h5>Patrick Odey</h5>
+          <p className="text-muted font-italic">Frontend Developer</p>
+        </div>
+        <div className="col-12 col-sm-12 col-md-4 col-lg-4">
+          <img src={team_2} alt={team_2} className="img-fluid team-img mb-2" />
+          <h5>Ankush Bhatia</h5>
+          <p className="text-muted font-italic">Social Media Expert</p>
+        </div>
+        <div className="col-12 col-sm-12 col-md-4 col-lg-4">
+          <img src={team_2} alt={team_2} className="img-fluid team-img mb-2" />
+          <h5>Andor E. Ofre</h5>
+          <p className="text-muted font-italic">Backend Developer</p>
         </div>
       </div>
     </div>
