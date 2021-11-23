599f523207a7b5014510e2bcc462f976acd89d13
Patrickodey360coder <patrickodey419@gmail.com>
2021-11-23 08:41:02 +0100

    styled validation error messages

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -24,19 +24,19 @@ function Contact() {
               <div className="col-10 col-sm-8 col-md-10 col-md-10 mx-auto">
               <form onSubmit={handleSubmit(onSubmit)} noValidate>
                 <div className="mb-3">
-                  <input className="form-control form-control-lg" type="text" placeholder="name..." aria-label=".form-control-lg example" {...register("name", {required: 'Please enter you name...'})}  />
-                  {errors.name && <p className="text-danger"> {errors.name.message}</p>}
+                  <input className="form-control form-control-lg" type="text" placeholder="name..." aria-label=".form-control-lg example" {...register("name", {required: 'name is required...'})}  />
+                  {errors.name && <small className="px-2 pt-5 text-danger"> {errors.name.message}</small>}
                 </div>
                 <div className="mb-3">
-                  <input type="email" class="form-control form-control-lg" id="exampleFormControlInput1" placeholder="email" {...register('email', {required: 'Please enter you email...', pattern: {
+                  <input type="email" class="form-control form-control-lg" id="exampleFormControlInput1" placeholder="email" {...register('email', {required: 'email is required...', pattern: {
                     value: /^(([^<>()\\[\]\\.,;:\s@"]+(\.[^<>()\\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                    message: 'Please enter a valid Email...'
+                    message: 'please enter a valid Email...'
                   } })}/>
-                  {errors.email && <p className="text-danger"> {errors.email.message}</p>}
+                  {errors.email && <small className="px-2 pt-5 text-danger"> {errors.email.message}</small>}
                 </div>
                 <div class="mb-3">
-                  <textarea placeholder="message..." class="form-control form-control-lg" id="exampleFormControlTextarea1" rows="3" {...register('message', {required: 'Please enter message...'})}></textarea>
-                  {errors.message && <p className="text-danger"> {errors.message.message}</p>}
+                  <textarea placeholder="message..." class="form-control form-control-lg" id="exampleFormControlTextarea1" rows="3" {...register('message', {required: 'message is required...'})}></textarea>
+                  {errors.message && <small className="px-2 pt-5 text-danger"> {errors.message.message}</small>}
                 </div>
 
                 <div className="mb-3 d-grid">
